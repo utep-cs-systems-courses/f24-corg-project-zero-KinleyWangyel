cfb2482b881e743dc28a0ae4a4ec537ab58ab379
Kinley T Wangyel <owner@OWNERs-MacBook-Pro-3.local>
2024-09-18 22:58:13 -0600

    update

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("don't Select which shape you want to print (Triangle = t, Square = s, Chars = c, Arrow = a, Newfont = f) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -18,7 +18,7 @@ int main()
     switch (c) {
     case 't':
       puts("You selected triangle:");
-      print_triangle(5, 7);
+      print_triangle(5, 6);
       break;
     case 's':
       puts("You selected square:");
@@ -28,6 +28,15 @@ int main()
       puts("You selected chars:");
       for (char c = 'a'; c < 'd'; c++)
 	print_char_5x7(c);
+      break;
+    case 'f':
+      puts("You selected new font");
+      for (char c = 'A'; c < 'D'; c++)
+	print_char_5x7(c);
+      break;
+    case 'a':
+      puts("You selected arrow:");
+      print_arrow(6,6);
       break;
     case 'q':
       puts("Bye!");
